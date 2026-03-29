adacd1a93f55202ed2f337e72a31c0502668e80e
SaiHarshitha-Jogu <saiharshitha975@gmail.com>
2026-03-29 23:25:13 +0530

    Simplify CORS handling with Flask-CORS
    Removed custom CORS headers function and used Flask-CORS instead.

--- DentalXrayAI/object_detector.py
+++ DentalXrayAI/object_detector.py
@@ -31,13 +31,6 @@ BASE_DIR = Path(__file__).parent.absolute()
 
 app = Flask(__name__)
 
-@app.after_request
-def apply_cors_headers(response):
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
-    response.headers["Access-Control-Allow-Methods"] = "GET,POST,OPTIONS"
-    return response
-
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Load model
